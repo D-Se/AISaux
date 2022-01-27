35779a1a48031e91978a08e0ba0fa24647316510
D-Se <2869830202@qq.com>
2022-01-27 17:35:35 +0800

    description

--- dev/drive_setup.R
+++ dev/drive_setup.R
@@ -1,1 +1,10 @@
-# googlesheets4::gs4_auth()
+# googlesheets4::gs4_auth(email = "2869830202@qq.com")
+# 
+# box::use(g = googlesheets4)
+# 
+# x = g$gs4_find("2869830202@qq.com")
+# 
+# x = googlesheets4::gs4_find()
+# 
+# 
+# g$gs4_get(x)
